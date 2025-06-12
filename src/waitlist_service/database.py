f32075438c8538c298ae5c9b421715efe33b9047
ebowwa <81942069+ebowwa@users.noreply.github.com>
2025-06-12 12:50:55 -0700

    Fix CI async test

--- src/waitlist_service/database.py
+++ src/waitlist_service/database.py
@@ -1,4 +1,4 @@
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import declarative_base
 from sqlalchemy.ext.asyncio import create_async_engine
 import os
 from supabase import create_client, Client
